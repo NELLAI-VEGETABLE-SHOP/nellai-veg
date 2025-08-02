0114c90f8844f7f97e72f3e4e533b654b5807b35
NELLAI-VEGETABLE-SHOP <nellaivegetableshop@gmail.com>
2025-08-02 16:01:37 +0530

    Update route.ts

--- app/api/razorpay/verify-payment/route.ts
+++ app/api/razorpay/verify-payment/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: NextRequest) {
   try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = await request.json()
 
-    const key_secret = process.env.RAZORPAY_KEY_SECRET || "MafoLpyJ9iJdqaKKKDf7HLkZ"
+    const key_secret = process.env.RAZORPAY_KEY_SECRET!
 
     // Create expected signature
     const body = razorpay_order_id + "|" + razorpay_payment_id
